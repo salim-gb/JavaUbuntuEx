6e231b6666d3671a66df299a4bd1b08b359d4d7f
Salim <anasalim28@gmail.com>
2021-06-19 22:00:17 +0300

    added new code

--- src/main/java/Exercise1/Main.java
+++ src/main/java/Exercise1/Main.java
@@ -2,6 +2,11 @@ package Exercise1;
 
 public class Main {
     public static void main(String[] args) {
-        
+        getSum(2,3);
+    }
+
+    private static void getSum(int a, int b) {
+        int c = a + b;
+        System.out.println(c);
     }
 }
